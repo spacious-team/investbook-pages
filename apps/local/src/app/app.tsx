2743b923621886102156c389641041c80b16ec0f
Evgenii Glazkov <ge@dpi4.com>
2026-02-26 18:36:30 +0300

    feat: initial layout added

--- apps/local/src/app/app.tsx
+++ apps/local/src/app/app.tsx
@@ -1,12 +1,41 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import DemoPage from './pages/DemoPage/DemoPage';
+import Navbar from 'widgets/Navbar/Navbar';
+import Sidebar from 'widgets/Sidebar/Sidebar';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainPage />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: 'demo',
+        element: <DemoPage />,
+      },
+    ],
   },
 ]);
+
+function Layout() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      
+      <div className="flex flex-col flex-1">
+        <Navbar />
+        
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
 
 export function App() {
   return <RouterProvider router={router} />;
---
+++ apps/local/src/app/widgets/Sidebar/Sidebar.tsx
@@ -0,0 +1,22 @@
+
+import { useState } from 'react';
+
+export default function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed(prev => !prev)
+  }
+
+  return (
+    <div className={`transition-all duration-300 ${collapsed ? 'w-16' : 'w-64'} overflow-hidden bg-gray-100 flex flex-col h-full`}>
+      <div className="h-15 bg-gray-300 flex items-center justify-center">
+      </div>
+      
+      <button onClick={onToggle} className="mt-auto p-4 w-full text-left hover:bg-gray-200 flex items-center gap-2">
+        <span className="text-lg">{collapsed ? '»' : '«'}</span>
+        {!collapsed && <span>Collapse</span>}
+      </button>
+    </div>
+  )
+}
