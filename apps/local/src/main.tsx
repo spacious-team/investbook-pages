31738b0ba4463c891e36ebf3bece528f5335108e
s.havrolev <s.havrolev@rdpe.ru>
2026-04-26 08:43:51 +0300

    feat: refactor folders

--- apps/local/src/main.tsx
+++ apps/local/src/main.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { configureApiClient } from '@investbook-pages/products';
 import App from './app/app';
-import { queryClient } from './app/query-client';
+import { queryClient } from './app/query-client/query-client';
 
 configureApiClient();
 
