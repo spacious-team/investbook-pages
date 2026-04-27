c1a89b4d9542110140a26e0b63e4b1dcc7e62ebd
s.havrolev <s.havrolev@rdpe.ru>
2026-04-27 19:23:39 +0300

    feat: renamefiles

--- libs/common-ui/src/lib/app-loader.tsx
+++ libs/common-ui/src/lib/app-loader.tsx
@@ -1,9 +1,9 @@
-import { Spinner } from './Spinner';
+import { Spinner } from './spinner';
 
 export function AppLoader() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/60 backdrop-blur-sm">
-      <Spinner className="size-10" />
+      <Spinner className="size-20" />
     </div>
   );
 }
